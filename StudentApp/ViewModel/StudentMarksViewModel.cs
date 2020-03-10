b6e1e9fb2c724e57bf87342f50192bf05582d092
Mohammad Ishaq <info@ishaq.io>
2020-03-10 15:28:40 -0500

    Added marks to table

---
+++ StudentApp/ViewModel/StudentMarksViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentApp.ViewModel
+{
+    public class StudentMarksViewModel
+    {
+        public int SubjectId { get; set; }
+        public int TotalMarks { get; set; }
+        public int ObtainedMarks { get; set; }
+        public decimal Percentage { get; set; }
+    }
+}
---
+++ StudentApp/ViewModel/StudentViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StudentApp.ViewModel
+{
+    public class StudentViewModel
+    {
+        public string StudentName { get; set; }
+        public string ClassName { get; set; }
+        public int ExamId { get; set; }
+        public string RollNumber { get; set; }
+        public List<StudentMarksViewModel>ListOfStudentMarksViewModel { get; set; }
+    }
+}
