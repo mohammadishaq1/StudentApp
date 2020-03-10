f04cd7a20efdcf081dde5caa2fd2d523a3a81c47
Mohammad Ishaq <info@ishaq.io>
2020-03-10 12:25:50 -0500

    Added UI modal for student

--- StudentApp/ViewModel/StudentMasterViewModel.cs
+++ StudentApp/ViewModel/StudentMasterViewModel.cs
@@ -13,11 +13,19 @@ namespace StudentApp.ViewModel
         public string Name { get; set; }
         [Display(Name = "Class Name")]
         public string Class { get; set; }
+        [Display(Name = "Roll Number")]
+        public string RollNumber { get; set; }
         [Display(Name ="Exam")]
         public int ExamId { get; set; }
         [Display(Name = "Subject")]
         public int SubjectId { get; set; }
         public IEnumerable<SelectListItem> ListOfExams { get; set; }
         public IEnumerable<SelectListItem> ListOfSubject { get; set; }
+        [Display(Name = "Total Marks")]
+        public int TotalMarks { get; set; }
+        [Display(Name = "Marks Obtained")]
+        public int MarksObtained { get; set; }
+        [Display(Name = "Percentage")]
+        public decimal Percentage { get; set; }
     }
 }
